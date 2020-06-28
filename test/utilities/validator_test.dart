aba3ffec777038b3ac5bd13f00137b2451fab3b7
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-06-28 16:29:31 +0900

    fixed validator test

--- test/utilities/validator_test.dart
+++ test/utilities/validator_test.dart
@@ -3,9 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Validator', () {
-    test('必須項目はnullではだめ', () {
-      expect(Validator.requiredValidator(null), Validator.requiredMessage);
-    });
     test('必須項目はから文字ではだめ', () {
       expect(Validator.requiredValidator(''), Validator.requiredMessage);
     });
@@ -13,11 +10,10 @@ void main() {
       expect(Validator.requiredValidator(''), null);
     });
     test('Emailアドレスは適当な文字列ではダメ', () {
-      expect(
-          Validator.requiredValidator('dfljsa'), Validator.invalidEmailMessage);
+      expect(Validator.emailValidator('dfljsa'), Validator.invalidEmailMessage);
     });
     test('普通のメールアドレスならオッケー', () {
-      expect(Validator.requiredValidator('dshukertjr@gmail.com'), null);
+      expect(Validator.emailValidator('dshukertjr@gmail.com'), null);
     });
   });
 }
