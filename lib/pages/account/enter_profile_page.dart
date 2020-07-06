c6cd5e20bd6c683b2d36a23bf23854ba7098862f
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-07-07 07:41:59 +0900

    creating the sexual orientation setting page in enter profile page

--- lib/pages/account/enter_profile_page.dart
+++ lib/pages/account/enter_profile_page.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:app/blocs/auth/auth_bloc.dart';
+import 'package:app/models/profile.dart';
 import 'package:app/utilities/validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
 class EnterProfilePage extends StatefulWidget {
@@ -35,6 +39,9 @@ class _EnterProfilePageState extends State<EnterProfilePage> {
   DateTime _birthDate;
   final _birthDateController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  File _profileImageFile;
+  SexualOrientation _sexualOrientation = SexualOrientation.blank();
+  PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -50,42 +57,120 @@ class _EnterProfilePageState extends State<EnterProfilePage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                key: EnterProfilePage.enterProfileNameTextFieldKey,
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'ニックネーム',
+      body: _body(context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _fab(),
+    );
+  }
+
+  RaisedButton _fab() {
+    return RaisedButton(
+      key: EnterProfilePage.enterProfilePageSubmitButtonKey,
+      onPressed: () {
+        if (!_formKey.currentState.validate()) {
+          return;
+        }
+        _pageController.nextPage(
+            duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+      },
+      child: Text('次へ'),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      children: <Widget>[
+        _page1(),
+        _page2(context),
+      ],
+    );
+  }
+
+  Widget _page2(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('あなたについて教えてください', style: Theme.of(context).textTheme.headline4),
+        Text('＊プロフィール上に表示されます'),
+        Row(
+          children: <Widget>[
+            Checkbox(
+              value: _sexualOrientation.maleG,
+              onChanged: (val) {
+                setState(() {
+                  _sexualOrientation = _sexualOrientation.copyWith(maleG: val);
+                });
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  /// Set profile image, name, and birthDate
+  Widget _page1() {
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Material(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: CircleBorder(),
+                child: InkWell(
+                  onTap: _pickProfileImage,
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: _profileImageFile == null
+                        ? Ink(
+                            color: Colors.blueGrey,
+                            child: Center(
+                              child: Icon(
+                                Icons.image,
+                                color: Colors.white.withOpacity(0.7),
+                              ),
+                            ),
+                          )
+                        : Ink.image(
+                            image: Image.file(_profileImageFile).image,
+                            fit: BoxFit.cover,
+                            child: Center(
+                              child: Icon(
+                                Icons.image,
+                                size: 40,
+                                color: Colors.white.withOpacity(0.7),
+                              ),
+                            ),
+                          ),
+                  ),
                 ),
-                validator: Validator.requiredValidator,
               ),
-              SizedBox(height: 24),
-              TextFormField(
-                key: EnterProfilePage.enterProfileBirthDateTextFieldKey,
-                readOnly: true,
-                onTap: _chooseBirthDate,
-                controller: _birthDateController,
-                decoration: InputDecoration(
-                  labelText: '生年月日',
-                ),
-                validator: Validator.requiredValidator,
+            ),
+            SizedBox(height: 24),
+            TextFormField(
+              key: EnterProfilePage.enterProfileNameTextFieldKey,
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'ニックネーム',
               ),
-              SizedBox(height: 24),
-              RaisedButton(
-                key: EnterProfilePage.enterProfilePageSubmitButtonKey,
-                onPressed: () {
-                  if (!_formKey.currentState.validate()) {
-                    return;
-                  }
-                },
-                child: Text('次へ'),
+              validator: Validator.requiredValidator,
+            ),
+            SizedBox(height: 24),
+            TextFormField(
+              key: EnterProfilePage.enterProfileBirthDateTextFieldKey,
+              readOnly: true,
+              onTap: _chooseBirthDate,
+              controller: _birthDateController,
+              decoration: InputDecoration(
+                labelText: '生年月日',
               ),
-            ],
-          ),
+              validator: Validator.requiredValidator,
+            ),
+            SizedBox(height: 24),
+          ],
         ),
       ),
     );
@@ -95,6 +180,7 @@ class _EnterProfilePageState extends State<EnterProfilePage> {
   void dispose() {
     _nameController.dispose();
     _birthDateController.dispose();
+    _pageController.dispose();
     super.dispose();
   }
 
@@ -109,9 +195,26 @@ class _EnterProfilePageState extends State<EnterProfilePage> {
       currentTime: _birthDate,
       locale: LocaleType.jp,
     );
-    setState(() {
-      _birthDate = birthDate;
-      _birthDateController.text = DateFormat('yyyy年MM月dd日').format(_birthDate);
-    });
+    if (birthDate != null) {
+      setState(() {
+        _birthDate = birthDate;
+        _birthDateController.text =
+            DateFormat('yyyy年MM月dd日').format(_birthDate);
+      });
+    }
+  }
+
+  void _pickProfileImage() async {
+    final selectedImageFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxHeight: 500,
+      maxWidth: 500,
+      imageQuality: 75,
+    );
+    if (selectedImageFile != null) {
+      setState(() {
+        _profileImageFile = File(selectedImageFile.path);
+      });
+    }
   }
 }
