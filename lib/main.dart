e54d9b95ba860963c28aa9394b584a3d2945ba2e
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-07-10 15:37:39 +0900

    created tabs

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'LGBT Match',
           theme: ThemeData(
+            scaffoldBackgroundColor: const Color(0xFFe9f5f8),
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity,
             inputDecorationTheme: const InputDecorationTheme(
